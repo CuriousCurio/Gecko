1d3b8c5371cae3e92da491926a9d0a7a1b22c729
CuriousCurio <116847106+CuriousCurio@users.noreply.github.com>
2022-11-22 13:18:06 -0700

    Update and rename README.md to README.lua

---
+++ README.lua
@@ -0,0 +1,7 @@
+-- Custom typeof
+local typeof = function(a)
+	local meta = getmetatable(a);
+	if (type(meta) ~= "table" or meta.__type == nil) then return typeof(a); end
+	if (type(meta.__type) ~= 'function') then return meta.__type; end
+	return meta.__type(a);
+end
