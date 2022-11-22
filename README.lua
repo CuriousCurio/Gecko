c872b0fac9fe22ac96af32f0f65c75e0c3e83054
CuriousCurio <116847106+CuriousCurio@users.noreply.github.com>
2022-11-22 13:20:34 -0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,7 +1,7 @@
 -- Custom typeof
 local typeof = function(a)
 	local meta = getmetatable(a);
-	if (type(meta) ~= "table" or meta.__type == nil) then return typeof(a); end
-	if (type(meta.__type) ~= 'function') then return meta.__type; end
+	if type(meta) ~= "table" or meta.__type == nil then return typeof(a); end
+	if type(meta.__type) ~= 'function' then return meta.__type; end
 	return meta.__type(a);
 end
